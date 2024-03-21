e2ca751b4d459e8d424158e5273080d1c2c97808
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-03-21 20:55:30 +0530

    virtual and override

--- example/Inheritance/ContractA.sol
+++ example/Inheritance/ContractA.sol
@@ -9,9 +9,16 @@ contract ContractA {
         owner = msg.sender;
     }
 
-    string public storedText = "Blockchain";
-    function changeText(string memory _text) external {
-        storedText = _text;
+    // These functions can be used inside any of imported inherted functions
+    function changeText(string memory _text) external pure returns(string memory){
+
+        return _text;
+    }
+
+    // This is a virtual function that can be used inherited override functions
+    function addNumbers(uint _number1, uint _number2) external virtual returns(uint){
+  
+        return _number1 + _number2;
     }
 
 }
--- example/Inheritance/ContractB.sol
+++ example/Inheritance/ContractB.sol
@@ -6,11 +6,34 @@ import "./ContractA.sol";
 
 // ContractB inherits from ContractA
 contract ContractB is ContractA {
-    // ContractB inherits all state variables and functions from ContractA
 
-    // No need to redefine the constructor, as it's already defined in ContractA
-    // ContractB inherits the constructor from ContractA
+    /** ----------------------------------------------------------------------------
+        - INHERITANCE
+            - ContractB inherits all state variables and functions from ContractA
+    
+            - No need to redefine the constructor, as it's already defined in ContractA
+            - ContractB inherits the constructor from ContractA
 
-    // No need to redefine storedText state variable or changeText function,
-    // as they are already defined in ContractA and inherited by ContractB
+            - No need to redefine changeText function,
+            - as they are already defined in ContractA and inherited by ContractB
+
+            - Can call any functions in ContractA after deploying this ContractB
+            - Simply we can use anything that inside ContractA in here
+    ----------------------------------------------------------------------------------
+    */
+
+    /** ----------------------------------------------------------------------------
+        - VIRTUAL and OVERRIDE Functions
+            - A virtual function is a function that a derived contract can override. 
+            - You can mark a function as virtual by using the virtual keyword. 
+            - If a derived contract overrides a virtual function, it must use the override keyword.
+            - The override keyword is used to explicitly indicate that a derived function is intended to override a virtual function in the base contract. 
+            - It ensures that the derived function has the same function signature as the virtual function it is overriding.
+    ----------------------------------------------------------------------------------
+    */
+    function addNumbers(uint _number1, uint _number2) external pure override returns(uint){
+        
+        return _number1 + _number2 + 10000;
+    }
+
 }
