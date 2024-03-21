53b6015bfef2238dfbdad2b31261fca743a5dd56
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-03-21 14:06:26 +0530

    Update Struct.sol with fruit struct and related functions

--- example/Struct.sol
+++ example/Struct.sol
@@ -1,16 +1,42 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.7;
+pragma solidity >=0.6.12 <0.9.0;
 
 contract Struct {
-    string public name = "Orange";
-    uint public price = 150;
-    bool public stock = true;
+    /** Normal Variables
+    * string public name = "Orange";
+    * uint public price = 150;
+    * bool public stock = true;
+    */
 
+    // Create model of the struct (tuple)
     struct Fruits {
         string name;
         uint price;
         bool stock;
     }
 
+    // Create a fruit1 Struct and store values
     Fruits public fruit1 = Fruits("Orange", 150, true);
+    
+    Fruits internal fruit2;
+    function createVar(string memory _name, uint _price, bool _stock) external {
+        fruit2 = Fruits(_name, _price, _stock);
+    }
+
+    function returnVar() external view returns(Fruits memory) {
+        return fruit2;
+    }
+
+    function updateVarPrice(uint _newPrice) external {
+        fruit2.price = _newPrice;
+    }
+    
+    function getVarPrice() external view returns(uint) {
+        return fruit2.price;
+    }
+
+    function changeVarStockStatus() external {
+        fruit2.stock = !fruit2.stock;
+    }
+
 }
