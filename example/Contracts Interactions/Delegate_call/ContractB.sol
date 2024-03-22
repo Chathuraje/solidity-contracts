7758cc67cac241f06198e2078e2998c7cfc8b18a
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-03-22 19:19:30 +0530

    Add public variable and update delegatecall function

--- example/Contracts Interactions/Delegate_call/ContractB.sol
+++ example/Contracts Interactions/Delegate_call/ContractB.sol
@@ -3,8 +3,8 @@ pragma solidity >=0.8.7;
 
 contract ContractB {
 
-    function delegatecall(address _otherContracAddress, string memory _word) external {
-            
+    string public text = "Hello, world!";
+    function delegatecall(address _otherContracAddress, string memory _word) external { 
             bytes memory data1;
             (bool success, bytes memory data) = _otherContracAddress.delegatecall(
                 abi.encodeWithSignature("changeText(string)", _word)
